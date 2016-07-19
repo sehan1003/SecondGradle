caf689d75e6057267ad1322cba3c8857d332330a
sh1003 <sehan1003@naver.com>
2016-07-19 17:12:37 +0900

    second label..

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -2,6 +2,8 @@ package com.hybrid;
 
 public class MainApplication {
 	public static void main(String[] args){
+		System.out.println("#####################");
 		System.out.println("This is Second Gradle");
+		System.out.println("#####################");
 	}
 }
