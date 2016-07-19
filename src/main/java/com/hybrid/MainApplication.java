a7d7b8f7306e85370da14759fd4e297faf4766aa
sh1003 <sehan1003@naver.com>
2016-07-19 17:14:26 +0900

    10ea Gradle

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -3,7 +3,8 @@ package com.hybrid;
 public class MainApplication {
 	public static void main(String[] args){
 		System.out.println("#####################");
-		System.out.println("This is Second Gradle");
+		for(int i=0; i<10; i++)
+			System.out.println("This is Second Gradle" + i);
 		System.out.println("#####################");
 	}
 }
